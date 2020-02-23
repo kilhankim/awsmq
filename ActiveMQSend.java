95b5af295a69c6ca048aba07d06356af1761b815
EC2 Default User <ec2-user@ip-10-0-1-160.ap-northeast-2.compute.internal>
2020-02-23 12:18:40 +0000

    java -classpath lib/activemq-all-5.15.11.jar:. ActiveMQSend <queue name> <file path & name> <think time> <iteration time>

--- ActiveMQSend.java
+++ ActiveMQSend.java
@@ -7,20 +7,51 @@ import javax.jms.Session;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.command.ActiveMQQueue;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Date;
 
 public class ActiveMQSend
 {
-  String receiveQueueName = "ActiveMQ";
+
+   BufferedReader br = null;
+   String sLine = null;
+   String tokenizedEnvString[] = null;
+   String readLine[] = null;
+   Date currentTime = null;
+   String dTime = null;
+   String fileName = null;
+ 
+   StringBuffer buff = new StringBuffer();
+   static String receiveQueueName = null;
+   static String sendQueueName = null;
+   static File envFile = null;
+   static int sleepTime = 0;
+   static int iterationCount = 0;
+    
+   public void init()
+   {
+	try { 
+		br = new BufferedReader(new FileReader(envFile));
+		while ((sLine = br.readLine()) != null) {
+                    buff.append(sLine);
+		}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+ 			
   
-  String sendQueueName = "ActiveMQ";
+       System.out.println(buff.toString());
+    }
+
   
   public void send() 
   //public void send() throws Exception
   {
     try{
-    System.out.println("check c");
     String url = "failover:(ssl://b-a827c188-6f38-4f76-9569-03e3046023fd-1.mq.ap-northeast-2.amazonaws.com:61617,ssl://b-a827c188-6f38-4f76-9569-03e3046023fd-2.mq.ap-northeast-2.amazonaws.com:61617)";
-    System.out.println("check d");
 
     //ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("tcp://localhost:61616");
     ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(url);
@@ -30,31 +61,33 @@ public class ActiveMQSend
 
 
 
-    System.out.println("check e");
     Connection connection = factory.createConnection();
     connection.start();
-    System.out.println("check f");
     Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
  
-    System.out.println("check 1");
     
     Queue queue = new ActiveMQQueue(sendQueueName);
-    System.out.println("check 1-2");
     MessageProducer producer = session.createProducer(queue);
-    System.out.println("check 2");
     Destination destination = session.createQueue(receiveQueueName);
-    System.out.println("check 3");
     
-    Message message = session.createTextMessage("MESSAGE 1");
-    message.setJMSReplyTo(destination);
-    producer.send(queue, message);
-    System.out.println("[SEND] " + message.toString());
-    
+
+    for(int i=0; i<iterationCount; i++)
+    {
+        //Message message = session.createTextMessage("MESSAGE jjouhiu");
+        Message message = session.createTextMessage(buff.toString());
+        message.setJMSReplyTo(destination);
+        producer.send(queue, message);
+        System.out.println("[SEND] " + message.toString());
+
+        Thread.sleep(1000);
+    }
+
+  /* 
     message = session.createTextMessage("MESSAGE 2");
     message.setJMSReplyTo(destination);
     producer.send(queue, message);
     System.out.println("[SEND] " + message.toString());
- 	
+ */	
     
     session.close();
     connection.close();
@@ -67,10 +100,18 @@ public class ActiveMQSend
   
   public static void main(String args[]) throws Exception
   {
-    System.out.println("check a");
+    if(args.length !=4)
+    {
+      System.out.println("Usage :  java -classpath lib/activemq-all-5.15.11.jar:. ActiveMQSend <queue name> <file path & name> <think time>"); 
+      return;
+    }
     ActiveMQSend qsr = new ActiveMQSend();
-    System.out.println("check b");
+    receiveQueueName = args[0].toString();
+    sendQueueName = args[0].toString();
+    envFile = new File(args[1].toString());
+    sleepTime = Integer.parseInt(args[2].toString());
+    iterationCount = Integer.parseInt(args[3].toString());
+    qsr.init();
     qsr.send();
-    System.out.println("check c");
   }
 } 
